e12a562bbb67898e56fc29c2fd0c583f24efa3b2
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-04-14 18:45:29 +0900

    Support Add and Sub

--- src/main.c
+++ src/main.c
@@ -10,13 +10,36 @@ int main(int argc, char **argv) {
   FILE *source_pointer = fopen(argv[1], "r");
   FILE *target_pointer = fopen("src.s", "w");
 
-  int num;
-  fscanf(source_pointer, "%d", &num);
-
   fprintf(target_pointer, ".intel_syntax noprefix\n");
   fprintf(target_pointer, ".global main\n");
   fprintf(target_pointer, "main:\n");
-  fprintf(target_pointer, "  mov rax, %d\n", num);
+
+  long first_num;
+  fscanf(source_pointer, "%ld", &first_num);
+
+  fprintf(target_pointer, "  mov rax, %ld\n", first_num);
+
+  char s[256];
+  fgets(s, 256, source_pointer);
+  char *p = &s;
+
+  while (*p != '\0' && *p != '\n') {
+    if (*p == '+') {
+      p++;
+      fprintf(target_pointer, " add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      fprintf(target_pointer, " sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexpected char: '%c'\n", *p);
+    return 1;
+  }
+
   fprintf(target_pointer, "  ret\n");
   return 0;
 }
