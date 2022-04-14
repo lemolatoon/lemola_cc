609f599185e884e6b81045051c60959846b88fd5
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-04-14 18:16:28 +0900

    Add test.sh

---
+++ test/test.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+assert() {
+    expected="$1"
+    input="$2"
+
+    echo $expected > tmp.c
+    ../lemola_cc tmp.c
+
+    cc -o tmp src.s 
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+assert 129 129
+
+echo OK
